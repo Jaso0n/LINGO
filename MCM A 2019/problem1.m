c120f5836b87d0f2c3858e917e3952b875faa6d0
Jaso0n <48013878+Jaso0n@users.noreply.github.com>
2019-05-25 08:44:10 +0800

    Add files via upload

--- MCM A 2019/problem1.m
+++ MCM A 2019/problem1.m
@@ -1,61 +1,54 @@
-function [F]=problem1() 
+function [a]=problem1() 
 clc;clear
 T =10;
 r = 0.08;
 investcost = 90;                          %90 * 10^6美元
-%% 第一种方法
-A1 = zeros(1,10);
+%% 第一种方案
 F1 = myf(r,T,10*investcost);       %第10年的等年值
-A1(1,:) = investym(r,T,F1);                %年金T取10
-plot(A1);
+A1 = investym(r,T,F1);           %年金T取10
 
-hold on
-%% 第二种方法
+%% 第二种方案
 F2 = 0;
 f = zeros(1,T);
 a = zeros(1,T);
-mats = zeros(T,T);
-
 for i = T:-1:1
     f(T-i+1) = myf(r,i,investcost);
-    a(T-i+1) = investym(r,i,f(T-i+1));
+    a(T-i+1) = investym(r,T,f(T-i+1));
     F2 =F2 + f(T-i+1);
 end
+A2 = sum(a);
 
-for i =1:T
-    mats(i,:) = a(i);
-end
-for i =1 :T
-    for j = 1:T 
-        if i>j
-            mats(i,j) =0;
-        end
-    end
-end
-A2 = sum(mats);
-plot(A2)
-
-%% 第三种方法
-first_invest = myf(r,T,5*investcost);
-last_invest = myf(r,1,5*investcost);
+%% 第三种方案
+p3 = 5*investcost;
+first_invest = myf(r,T,p3);
+last_invest = myf(r,1,p3);
 a_f = investym(r,T,first_invest);
-a_l = investym(r,1,last_invest);
-A3 = zeros(2,10);
-A3(1,:) = a_f;
-A3(2,10) = a_l; 
-A3 = sum(A3);
+a_l = investym(r,T,last_invest);
+A3 = a_f + a_l;
 F3 =first_invest + last_invest;
-plot(A3);
-legend('方法一','方法二','方法三')
-title('每年投资费用')
+
+%% 画图
+A = zeros(3,T);
+A(1,:) = A1;
+A(2,:) = A2;
+A(3,:) = A3;
+X = 1:10;
+hold on
+for i = 1:3
+    plot(X,A(i,:));
+end
+legend('方案一','方案二','方案三');
+title('年投资额')
 xlabel('年')
-ylabel('投资费用')
-hold off
+ylabel('投资额')
+
 F = [F1 F2 F3];
+hold off
 figure
 bar(F,'b')
-xlabel('方法')
-ylabel('投资费用')
+title('折算到第10年的总投资额')
+xlabel('方案')
+ylabel('投资额')
 end
 
 function f = myf(r,t,p)
